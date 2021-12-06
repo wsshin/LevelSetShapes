79e9095631e778a8c4cc423fe3a767b6e2f6123e
Wonseok Shin <wsshin97@gmail.com>
2021-12-06 17:00:10 -0500

    Attempt to make newton() work even for singular f′ₙ

--- src/util.jl
+++ src/util.jl
@@ -3,6 +3,8 @@ export newton, lagrange
 newton(f, x₀::Real; kwargs...) = newton_impl(f, x₀, derivative, abs; kwargs...)
 newton(f, x₀::SReal; kwargs...) = newton_impl(f, x₀, jacobian, norm; kwargs...)
 
+
+
 function newton_impl(f, x₀,
                      jfun,  # jacobian function
                      nfun;  # norm function
@@ -13,6 +15,7 @@ function newton_impl(f, x₀,
     # Solve f(x) = 0 using the Newton-Armijo method.
     # x₀: initial guess
     # f: function of x
+    K = Size(x₀)[1]
 
     isconverged = true  # true if solution converged; false otherwise
     α = 1e-4
@@ -32,10 +35,13 @@ function newton_impl(f, x₀,
         n_ls = 0  # line search iteration counter
 
         f′ₙ = jfun(f, xₙ)
+        bool_nz = SVec(ntuple(k->!iszero(f′ₙ[:,k]), Val(K)))
+        M = sum(bool_nz)
+        ind_nz = SVec{M}((1:K)[bool_nz])
 
         local ∆x  # see https://discourse.julialang.org/t/cumbersome-scoping-rules-for-try-catch-finally/4582
         try
-            ∆x = -f′ₙ \ fₙ
+            ∆x = -f′ₙ[:,ind_nz] \ fₙ
         catch err
             isa(err,SingularException) && @error "For xₙ = $xₙ at n = $n, f′ₙ = $f′ₙ is singular."
             break
@@ -43,22 +49,25 @@ function newton_impl(f, x₀,
 
         # Avoid too large Newton steps.
         nfun(∆x) ≤ ∆max || (isconverged = false; break)
-        xₙ₊₁ = xₙ + λ*∆x
+        # xₙ₊₁ = xₙ + λ*∆x
+        xₙ₊₁′ = MVec(xₙ)
+        xₙ₊₁′[ind_nz] .+= λ .* ∆x
+        xₙ₊₁ = SVec(xₙ₊₁′)
         fₙ₊₁ = f(xₙ₊₁)
 
-        # Perform the line search to determine λ.  The stopping criterion does not
-        # have perturb_ls ≈ 1e-12 on the RHS, but I guess this kind of perturbation
-        # allows update in xₙ even in the situation where line search is supposed
-        # to fail.
-        while nfun(fₙ₊₁) ≥ (1 - α*λ) * nfun(fₙ) + perturb_ls
-            λ /= 2
-            xₙ₊₁ = xₙ + λ*∆x
-            fₙ₊₁ = f(xₙ₊₁)
-            n_ls += 1
-
-            # Too many iteration steps in line search
-            n_ls ≤ maxit_ls || (isconverged = false; break)
-        end
+        # # Perform the line search to determine λ.  The stopping criterion does not
+        # # have perturb_ls ≈ 1e-12 on the RHS, but I guess this kind of perturbation
+        # # allows update in xₙ even in the situation where line search is supposed
+        # # to fail.
+        # while nfun(fₙ₊₁) ≥ (1 - α*λ) * nfun(fₙ) + perturb_ls
+        #     λ /= 2
+        #     xₙ₊₁ = xₙ + λ*∆x
+        #     fₙ₊₁ = f(xₙ₊₁)
+        #     n_ls += 1
+        #
+        #     # Too many iteration steps in line search
+        #     n_ls ≤ maxit_ls || (isconverged = false; break)
+        # end
 
         # Step accepted; continue the Newton method.
         xₙ = xₙ₊₁
