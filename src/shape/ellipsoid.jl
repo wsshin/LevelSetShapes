1720ffe4fac41512a73d7f00214de8ddc9bf3da2
Wonseok Shin <wsshin97@gmail.com>
2025-05-24 15:58:36 -0400

    Remove usages of AbbreviatedTypes

--- src/shape/ellipsoid.jl
+++ src/shape/ellipsoid.jl
@@ -3,13 +3,13 @@ export Ellipsoid, Ball
 struct Ellipsoid{K,K²} <: AbstractShape{K}
     c::SVector{K,Float64}  # center of ellipsoid
     r::SVector{K,Float64}  # semiaxes ("radii") in axis directions
-    p::S²Float{K,K²}  # projection matrix to Ellipsoid coordinates; must be orthonormal (see surfpt_nearby)
+    p::SMatrix{K,K,Float64,K²}  # projection matrix to Ellipsoid coordinates; must be orthonormal (see surfpt_nearby)
     Ellipsoid{K,K²}(c,r,p) where {K,K²} = new(c,r,p)  # suppress default outer constructor
 end
 
 function Ellipsoid(c::AbstractVector{<:Real},  # center of ellipsoid
                    r::AbstractVector{<:Real},  # semiaxes ("radii")
-                   axes::AbsMatReal=S²Float{length(c)}(I))  # columns are axes vector; assumed orthogonal
+                   axes::AbstractMatrix{<:Real}=SMatrix{length(c),length(c)}(I))  # columns are axes vector; assumed orthogonal
     K = length(c)
     l_ax = SVector(ntuple(k->norm(@view(axes[:,k])), Val(K)))
     p = axes' ./ l_ax
@@ -17,7 +17,7 @@ function Ellipsoid(c::AbstractVector{<:Real},  # center of ellipsoid
     return Ellipsoid{K,K*K}(c,r,p)
 end
 
-Ball(c::AbsVecReal, r::Real) = (K = length(c); Ellipsoid(c, @SVector(fill(r,K))))
+Ball(c::AbstractVector{<:Real}, r::Real) = (K = length(c); Ellipsoid(c, @SVector(fill(r,K))))
 
 Base.:(==)(s1::Ellipsoid, s2::Ellipsoid) = s1.c==s2.c && s1.r==s2.r && s1.p==s2.p
 Base.isapprox(s1::Ellipsoid, s2::Ellipsoid) = s1.c≈s2.c && s1.r≈s2.r && s1.p≈s2.p
