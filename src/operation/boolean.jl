e622a651ea395038472a64f3a11565c1b3f5e37f
Wonseok Shin <wsshin97@gmail.com>
2025-05-24 15:47:49 -0400

    In IntersectionShape, rename svec to shps; remove center(...)

--- src/operation/boolean.jl
+++ src/operation/boolean.jl
@@ -1,11 +1,10 @@
 struct IntersectionShape{K,S<:AbstractShape{K}} <: AbstractShape{K}
     shps::Vector{S}
 
-    IntersectionShape{K}(svec) where {K} = new(shps)
+    IntersectionShape{K}(shps) where {K} = new(shps)
 end
 
-CompositeShape(c::AbstractVector{<:Real}, s::AbstractShape...) = CompositeShape(c, [s...])
-CompositeShape(c::AbstractVector{<:Real}, svec::AbsVec{<:AbstractShape}) = CompositeShape{length(c)}(c,svec)
+CompositeShape(shps::AbstractShape...) = CompositeShape(SVector(shps))
+CompositeShape(shps::AbstractVector{<:AbstractShape}) = CompositeShape{length(shps)}(shps)
 
-level(x::SVector{K,<:Real}, s::CompositeShape{K}) where {K} = mapreduce(sᵢ->level(x,sᵢ), max, s.svec; init=-Inf)
-center(s::CompositeShape) = s.c
+level(x::SVector{K,<:Real}, shp::CompositeShape{K}) where {K} = mapreduce(shpᵢ->level(x,shpᵢ), max, shp.shps; init=-Inf)
