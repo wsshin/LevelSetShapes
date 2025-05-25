d36e3467fc072f5aa5b021ece99f5bfde4a01bde
Wonseok Shin <wsshin97@gmail.com>
2025-05-25 13:10:06 -0400

    Rename CompositeShape to IntersectionShape

--- src/operation/boolean.jl
+++ src/operation/boolean.jl
@@ -1,10 +1,8 @@
 struct IntersectionShape{K,S<:AbstractShape{K}} <: AbstractShape{K}
     shps::Vector{S}
-
-    IntersectionShape{K}(shps) where {K} = new(shps)
 end
 
-CompositeShape(shps::AbstractShape...) = CompositeShape(SVector(shps))
-CompositeShape(shps::AbstractVector{<:AbstractShape}) = CompositeShape{length(shps)}(shps)
+IntersectionShape(shps::AbstractShape...) = IntersectionShape(SVector(shps))
+IntersectionShape(shps::AbstractVector{<:AbstractShape}) = IntersectionShape{length(shps),eltype(shps)}(shps)
 
-level(x::SVector{K,<:Real}, shp::CompositeShape{K}) where {K} = mapreduce(shpᵢ->level(x,shpᵢ), max, shp.shps; init=-Inf)
+level(x::SVector{K,<:Real}, shp::IntersectionShape{K}) where {K} = mapreduce(shpᵢ->level(x,shpᵢ), max, shp.shps; init=-Inf)
