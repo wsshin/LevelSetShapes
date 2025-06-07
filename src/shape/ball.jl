65adbb4efcd4cf61e731d3b227f1c9582a10ca9b
Wonseok Shin <wsshin97@gmail.com>
2025-06-07 10:58:21 -0400

    Remove level(...) with δr from smooth shapes

--- src/shape/ball.jl
+++ src/shape/ball.jl
@@ -9,6 +9,6 @@ Ball(c::AbstractVector{<:Real}, r::Real) = Ball{length(c)}(c,r)
 isapprox(s1::Ball, s2::Ball) = s1.c≈s2.c && s1.r≈s2.r
 hash(s::Ball, h::UInt) = hash(s.c, hash(s.r, hash(:Ball, h)))
 
-level(x::SVector{K,<:Real}, s::Ball{K}, ∆r::Real) where {K} = norm(x-s.c) - (s.r - ∆r)  # signed distance function
+level(x::SVector{K,<:Real}, s::Ball{K}) where {K} = norm(x-s.c) - s.r  # signed distance function
 center(s::Ball) = s.c
 bounds(s::Ball) = (s.c .- s.r, s.c .+ s.r)
