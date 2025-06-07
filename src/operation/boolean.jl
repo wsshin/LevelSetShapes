11ab82d1a1c1287822edad40109ef03db373451f
Wonseok Shin <wsshin97@gmail.com>
2025-06-07 10:59:13 -0400

    First working implementation of level-set functions for rounded shapes

--- src/operation/boolean.jl
+++ src/operation/boolean.jl
@@ -5,4 +5,19 @@ end
 IntersectionShape(shps::AbstractShape...) = IntersectionShape(SVector(shps))
 IntersectionShape(shps::AbstractVector{<:AbstractShape}) = IntersectionShape{length(shps),eltype(shps)}(shps)
 
-level(x::SVector{K,<:Real}, shp::IntersectionShape{K}) where {K} = mapreduce(shpᵢ->level(x,shpᵢ), max, shp.shps; init=-Inf)
+function level(x::SVector{K,<:Real}, shp::IntersectionShape{K}, δr::Real=0) where {K}
+    lv_sharp = mapreduce(shpᵢ->level(x,shpᵢ), max, shp.shps; init=-Inf)
+    if lv_sharp < -δr
+        lv_round = lv_sharp
+    else
+        lv_round = zero(lv_sharp)
+        for shpᵢ = shp.shps
+            lvᵢ_recess = level(x, shpᵢ) + δr
+            dᵢ = max(0, lvᵢ_recess)
+            lv_round += dᵢ^2
+        end
+        lv_round = √lv_round - δr
+    end
+
+    return lv_round
+end
